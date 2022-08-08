15d78ea7016ec890c30e0cb72cac6afa02d3e619
reza <rezahajipour@gmail.com>
2022-08-08 23:10:48 +0200

    nav config

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -8,42 +8,34 @@ import Typography from "@mui/material/Typography";
 import Menu from "@mui/material/Menu";
 import MenuIcon from "@mui/icons-material/Menu";
 import Container from "@mui/material/Container";
-import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
-import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
 import AdbIcon from "@mui/icons-material/Adb";
+import { makeStyles } from "@mui/styles";
 
-const pages = ["Products", "Pricing", "Blog"];
-const settings = ["Profile", "Account", "Dashboard", "Logout"];
+const pages = ["اخبار", "اصلاعات عمومی آلمان", "اقامت", "تحصیل"];
 
 const Nav = () => {
+    const classes = useStyles();
     const [anchorElNav, setAnchorElNav] = React.useState(null);
-    const [anchorElUser, setAnchorElUser] = React.useState(null);
 
     const handleOpenNavMenu = (event) => {
         setAnchorElNav(event.currentTarget);
-    };
-    const handleOpenUserMenu = (event) => {
-        setAnchorElUser(event.currentTarget);
     };
 
     const handleCloseNavMenu = () => {
         setAnchorElNav(null);
     };
 
-    const handleCloseUserMenu = () => {
-        setAnchorElUser(null);
-    };
-
     return (
         <AppBar position="static">
-            <Container maxWidth="xl">
+            <Container maxWidth="xl" className={classes.mainContainer}>
                 <Toolbar disableGutters>
                     <AdbIcon
                         sx={{ display: { xs: "none", md: "flex" }, mr: 1 }}
                     />
                     <Typography
+                        className={classes.logo}
                         variant="h6"
                         noWrap
                         component="a"
@@ -58,7 +50,7 @@ const Nav = () => {
                             textDecoration: "none",
                         }}
                     >
-                        LOGO
+                        Alman Online
                     </Typography>
 
                     <Box
@@ -126,7 +118,7 @@ const Nav = () => {
                             textDecoration: "none",
                         }}
                     >
-                        LOGO
+                        Alman Online (m)
                     </Typography>
                     <Box
                         sx={{
@@ -144,47 +136,6 @@ const Nav = () => {
                             </Button>
                         ))}
                     </Box>
-
-                    <Box sx={{ flexGrow: 0 }}>
-                        <Tooltip title="Open settings">
-                            <IconButton
-                                onClick={handleOpenUserMenu}
-                                sx={{ p: 0 }}
-                            >
-                                <Avatar
-                                    alt="Remy Sharp"
-                                    src="/static/images/avatar/2.jpg"
-                                />
-                            </IconButton>
-                        </Tooltip>
-                        <Menu
-                            sx={{ mt: "45px" }}
-                            id="menu-appbar"
-                            anchorEl={anchorElUser}
-                            anchorOrigin={{
-                                vertical: "top",
-                                horizontal: "right",
-                            }}
-                            keepMounted
-                            transformOrigin={{
-                                vertical: "top",
-                                horizontal: "right",
-                            }}
-                            open={Boolean(anchorElUser)}
-                            onClose={handleCloseUserMenu}
-                        >
-                            {settings.map((setting) => (
-                                <MenuItem
-                                    key={setting}
-                                    onClick={handleCloseUserMenu}
-                                >
-                                    <Typography textAlign="center">
-                                        {setting}
-                                    </Typography>
-                                </MenuItem>
-                            ))}
-                        </Menu>
-                    </Box>
                 </Toolbar>
             </Container>
         </AppBar>
@@ -193,8 +144,12 @@ const Nav = () => {
 
 export default Nav;
 
-// const Nav = () => {
-//     return <div className="navContainer">Nav container</div>;
-// };
-
-// export default Nav;
+const useStyles = makeStyles(() => ({
+    mainContainer: {
+        background: "#111111",
+        color: "#E65C4F",
+    },
+    logo: {
+        color: "#E65C4F",
+    },
+}));
---
+++ client/src/theme.js
@@ -0,0 +1,55 @@
+import { createMuiTheme, responsiveFontSizes } from "@material-ui/core/styles";
+
+let theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: "#d75f5f",
+        },
+        secondary: {
+            main: "#a3ccc3",
+        },
+        text: {
+            primary: "#232F37",
+            secondary: "#ffffff",
+        },
+    },
+    typography: {
+        fontSize: 14,
+        h2: {
+            fontWeight: 700,
+        },
+        body1: {
+            fontWeight: 300,
+        },
+    },
+    overrides: {
+        MuiButton: {
+            label: {
+                color: "#ffffff",
+            },
+            contained: {
+                border: "2px solid #d75f5f",
+            },
+            outlined: {
+                border: "2px solid #ffffff",
+            },
+        },
+        MuiStepper: {
+            root: {
+                backgroundColor: "transparent",
+            },
+        },
+        MuiStepIcon: {
+            active: {
+                color: "#d75f5f",
+            },
+            completed: {
+                color: "#a3ccc3 !important",
+            },
+        },
+    },
+});
+
+theme = responsiveFontSizes(theme);
+
+export default theme;
